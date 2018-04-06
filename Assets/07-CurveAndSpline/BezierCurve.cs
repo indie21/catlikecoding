887ef2ece5a2cc1a1c46c8aedbc1e23888696fdc
kangge <zhuoyikang@gmail.com>
2018-04-06 10:10:15 +0800

    bezier curve

--- Assets/07-CurveAndSpline/BezierCurve.cs
+++ Assets/07-CurveAndSpline/BezierCurve.cs
@@ -11,24 +11,24 @@ public class BezierCurve : MonoBehaviour
             new Vector3(1f, 0f, 0f),
             new Vector3(2f, 0f, 0f),
             new Vector3(3f, 0f, 0f),
+            new Vector3(4f, 0f, 0f),
         };
     }
 
-
     public Vector3 GetPoint(float t)
     {
-        return transform.TransformPoint(Bezier.GetPoint(_points[0], _points[1], _points[2], t));
+        return transform.TransformPoint(Bezier.GetPoint(_points[0], _points[1], _points[2], _points[3], t));
     }
 
-	public Vector3 GetVelocity(float t)
-	{
-		return transform.TransformPoint(Bezier.GetFirstDerivative(_points[0], _points[1], _points[2], t)) -
-			transform.position;
-	}
+    public Vector3 GetVelocity(float t)
+    {
+        return transform.TransformPoint(Bezier.GetFirstDerivative(_points[0], _points[1], _points[2], _points[3], t)) -
+        transform.position;
+    }
 
-	public Vector3 GetDirection(float t)
-	{
-		return GetVelocity(t).normalized;
-	}
+    public Vector3 GetDirection(float t)
+    {
+        return GetVelocity(t).normalized;
+    }
 
 }
--- Assets/07-CurveAndSpline/Editor/BezierCurveInspector.cs
+++ Assets/07-CurveAndSpline/Editor/BezierCurveInspector.cs
@@ -21,28 +21,27 @@ public class BezierCurveInspector : Editor
         Vector3 p0 = ShowPoint(0);
         Vector3 p1 = ShowPoint(1);
         Vector3 p2 = ShowPoint(2);
+		Vector3 p3 = ShowPoint(3);
 
         Handles.color = Color.gray;
         Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p1, p2);
+        Handles.DrawLine(p2, p3);
 
-        Handles.color = Color.white;
-        Vector3 lineStart = _curve.GetPoint(0f);
-		Handles.color = Color.green;
-		Handles.DrawLine(lineStart, lineStart+_curve.GetDirection(0f));
-
-        for (int i = 1; i <= _lineStep; i++)
-        {
-            Vector3 lineEnd = _curve.GetPoint(i / (float)_lineStep);
-			Handles.color = Color.white;
-            Handles.DrawLine(lineStart, lineEnd);
-
-			Handles.color = Color.green;
-			Handles.DrawLine(lineEnd, lineEnd + _curve.GetDirection(i / (float)_lineStep));
-            lineStart = lineEnd;
-        }
+		ShowDirections();
+		Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
 
     }
+
+	private void ShowDirections()
+	{
+		Handles.color = Color.green;
+		Vector3 point = _curve.GetPoint(0f);
+		Handles.DrawLine(point, point + _curve.GetDirection(0f) * _directionScale);
+		for (int i = 1; i <= _lineStep; i++) {
+			point = _curve.GetPoint(i / (float)_lineStep);
+			Handles.DrawLine(point, point + _curve.GetDirection(i / (float)_lineStep) * _directionScale);
+		}
+	}
 
 
     private Vector3 ShowPoint(int index)
