69fa97d3ffe9cef98578b452ec0502e76c1ffc3d
kangge <zhuoyikang@gmail.com>
2018-04-22 11:20:27 +0800

    load and save

---
+++ Assets/08-ProceduralGrid/Grid.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour {
+
+	private Mesh _mesh;
+	public int _xSize, _ySize;
+	private Vector3[] _vertices;
+
+	private void Awake () {
+		StartCoroutine(Generate());
+	}
+
+	private IEnumerator Generate () {
+		var wait = new WaitForSeconds(0.02f);
+
+		GetComponent<MeshFilter>().mesh = _mesh = new Mesh();
+		_mesh.name = "Procedural Grid";
+
+		_vertices = new Vector3[(_xSize + 1) * (_ySize + 1)];
+
+		for (int i = 0, y = 0; y <= _ySize; y++) {
+			for (int x = 0; x <= _xSize; x++, i++) {
+				_vertices[i] = new Vector3(x, y);
+				yield return wait;
+			}
+		}
+
+		_mesh.vertices = _vertices;
+
+		int[] triangles = new int[_xSize * _ySize * 6];
+		for (int ti = 0, vi = 0, y = 0; y < _ySize; y++, vi++) {
+			for (int x = 0; x < _xSize; x++, ti += 6, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+				triangles[ti + 4] = triangles[ti + 1] = vi + _xSize + 1;
+				triangles[ti + 5] = vi + _xSize + 2;
+			}
+		}
+
+		_mesh.triangles = triangles;
+		_mesh.RecalculateNormals();
+
+	}
+
+	private void OnDrawGizmos () {
+		if (_vertices == null) {
+			return;
+		}
+
+		Gizmos.color = Color.black;
+		for (int i = 0; i < _vertices.Length; i++) {
+			Gizmos.DrawSphere(_vertices[i], 0.1f);
+		}
+	}
+
+
+}
---
+++ Assets/09-PersistingObjects/Game.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    public Transform _prefeb;
+    public KeyCode _createKey = KeyCode.C;
+    public KeyCode _newGameKey = KeyCode.N;
+    public KeyCode _saveGameKey = KeyCode.S;
+    public KeyCode _loadGameKey = KeyCode.L;
+
+    List<Transform> _objects;
+    string _savePath;
+
+    void Awake()
+    {
+        _objects = new List<Transform>();
+        _savePath = Path.Combine(Application.persistentDataPath, "saveFile");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(_createKey))
+        {
+            CreateObject();
+        }
+        else if (Input.GetKeyDown(_newGameKey))
+        {
+            BeginNewGame();
+        }
+        else if (Input.GetKeyDown(_saveGameKey))
+        {
+            Save();
+        }
+        else if (Input.GetKeyDown(_loadGameKey))
+        {
+            Load();
+        }
+    }
+
+    void CreateObject()
+    {
+        Transform t = Instantiate(_prefeb);
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+        _objects.Add(t);
+    }
+
+    void BeginNewGame()
+    {
+        for (int i = 0; i < _objects.Count; i++)
+        {
+            Destroy(_objects[i].gameObject);
+        }
+        _objects.Clear();
+    }
+
+    void Save()
+    {
+        using (var writer = new BinaryWriter(File.Open(_savePath, FileMode.Create)))
+        {
+            writer.Write(_objects.Count);
+            foreach (var t in _objects)
+            {
+                writer.Write(t.localPosition.x);
+                writer.Write(t.localPosition.y);
+                writer.Write(t.localPosition.z);
+            }
+        }
+    }
+
+    void Load()
+    {
+        BeginNewGame();
+        using (var reader = new BinaryReader(File.Open(_savePath, FileMode.Open)))
+        {
+            int count = reader.ReadInt32();
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 p;
+                p.x = reader.ReadSingle();
+                p.y = reader.ReadSingle();
+                p.z = reader.ReadSingle();
+
+                var t = Instantiate(_prefeb);
+                t.localPosition = p;
+                _objects.Add(t);
+            }
+        }
+    }
+
+}
